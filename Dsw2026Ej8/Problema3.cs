692e9ce602ad5a3195d25ebd8c5fbbf476ac6f02
AgustinCarabajal <enrique.carabajal@alu.frt.utn.edu.ar>
2026-05-04 21:23:55 -0300

    agregar ejercicio 3, archivo program modificado otra vez, agregarclases product y problema3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+       public string CompararCopias(int originalValue, Product product)
+       {
+            int valorCopia = originalValue;
+            valorCopia = Incrementar(valorCopia);
+
+                                                                                                                                                    Product productoCopia = product;
+            string nuevaDescripcion = GenerarDescripcionModificada(productoCopia.Description);
+            productoCopia.ActualizarDescripcion(nuevaDescripcion);
+
+            return $"{originalValue}-{valorCopia}-{product.Description}";
+        }
+
+        private int Incrementar(int valor){  
+            return valor + 1; 
+        }
+        private string GenerarDescripcionModificada(string descripcionOriginal)
+        {
+            return $"{descripcionOriginal} [modificado]";
+        }
+            
+    }
+}
+
+    
+
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -5,18 +5,26 @@
         static void Main(string[] args)
         {
             //pruebas para el problema 1            
+            Console.WriteLine("\nproblema 1:");
             ProductHelper helper = new ProductHelper();
+
 
             Console.WriteLine(helper.ObtenerEtiquetaProducto(1001, "Leche", 1250.00m));
             Console.WriteLine(helper.ObtenerEtiquetaProducto(2048, "Arroz", 980.50m));
             Console.WriteLine(helper.ObtenerEtiquetaProducto(3999, "Aceite de oliva", 2375.99m));
 
             //pruebas para el problema 2
-            Console.WriteLine("=== Problema 2: CrearResumenVenta ===");
+            Console.WriteLine("\nproblema 2:");
             var p2 = new Problema2();
             Console.WriteLine(p2.CrearResumenVenta(101, "Harina 1kg", 3, 450.00m));  
             Console.WriteLine(p2.CrearResumenVenta(102, "Azúcar 500g", 0, 300.00m)); 
-            Console.WriteLine(p2.CrearResumenVenta(103, "Yerba 500g", -1, 800.00m)); 
+            Console.WriteLine(p2.CrearResumenVenta(103, "Yerba 500g", -1, 800.00m));
+
+            //pruebas para el problema 3
+            Console.WriteLine("\nproblema 3:");
+            var p3 = new Problema3();
+            var product = new Product("Notebook Lenovo");
+            Console.WriteLine(p3.CompararCopias(10, product));
         }
     }
 }
